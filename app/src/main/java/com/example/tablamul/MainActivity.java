fccd8cbc18d8c554c10db3eff0c9766762626e7e
juanka2023 <bull-ter@hotmail.es>
2023-04-11 09:21:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tablamul/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.tablamul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etnumero;
+    private TextView etsalida;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etnumero = (EditText) findViewById(R.id.etnumero);
+        etsalida = (TextView) findViewById(R.id.etsalida);
+    }
+
+    public void calcular(View view) {
+        String cad = "";
+        if (etnumero.getText().toString().equals("")) {
+            Toast.makeText(this, "ingrese un numero", Toast.LENGTH_LONG).show();
+        } else {
+            int numero = Integer.parseInt(etnumero.getText().toString());
+            for (int i = 1; i <= 10; i++) {
+                cad += String.valueOf(numero) + "\t x \t" + i + " \t = \t" + (numero * i) + "\n";
+            }
+            etsalida.setText(cad);
+        }
+    }
+    public void irArchivo(View view){
+        Intent i = new Intent(this, activityarchivo.class);
+        startActivity(i);
+    }
+    public void  guardar (View view){
+        String nombre =  etnumero.getText().toString();
+        String contenido = etsalida.getText().toString();
+
+        try {
+            //donde se encuentra la tarjeta SD
+            File tarjetaSD = Environment.getExternalStorageDirectory();
+            Toast.makeText(this, tarjetaSD.getPath(), Toast.LENGTH_SHORT).show();
+            /// creamos el archivo con el nombre que nos indica el archivo, y el Pach de la tarjera SD
+            File rutaArchivo = new File(tarjetaSD.getPath(),nombre );
+
+            /// abrimos el archivo
+            OutputStreamWriter CrearArchivo = new OutputStreamWriter(openFileOutput(nombre, Activity.MODE_PRIVATE));
+
+            ///guarrdamos los datos del atchivo
+            CrearArchivo.write(contenido);
+            //limpiamos el Buffer
+            CrearArchivo.flush();
+            CrearArchivo.close();
+
+            Toast.makeText(this, "se ha guardado correctamente", Toast.LENGTH_SHORT).show();
+            etnumero.setText("");
+            etsalida.setText("");
+        } catch (IOException e) {
+            Toast.makeText(this, "no se pudo guardar el archivo", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tablamul/activityarchivo.java
@@ -0,0 +1,57 @@
+package com.example.tablamul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class activityarchivo extends AppCompatActivity {
+
+    private EditText et1buscar;
+    private TextView tvresul;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityarchivo);
+        et1buscar = (EditText) findViewById(R.id.etbuscar);
+        tvresul = (TextView) findViewById(R.id.tvresultado);
+    }
+    public void consultar(View view){
+        String nombre = et1buscar.getText().toString();
+
+        try {
+            File tarjetaSD = Environment.getExternalStorageDirectory();
+            File rutaArchivo = new File(tarjetaSD.getPath(),nombre);
+            InputStreamReader abrirArchivo = new InputStreamReader(openFileInput(nombre));
+
+            BufferedReader leerArchivo = new BufferedReader(abrirArchivo);
+            String linea = leerArchivo.readLine();
+            String contenidoCompleto = "";
+
+            while (linea != null){
+                contenidoCompleto = contenidoCompleto + linea +"\n";
+            }
+            leerArchivo.close();
+            abrirArchivo.close();
+            tvresul.setText(contenidoCompleto);
+
+        }catch (IOException e){
+            Toast.makeText(this, "error al leer el archivo", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void volver(View view){
+     finish();
+    }
+}
